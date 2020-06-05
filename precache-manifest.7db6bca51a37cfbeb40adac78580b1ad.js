27d204fc3b7f0e583a6a96edc0c4848212cd4ebb
unknown <Elior Hazan>
2020-06-05 13:20:46 +0300

    Updates

---
+++ precache-manifest.7db6bca51a37cfbeb40adac78580b1ad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e57bc602377a58e373d0dcc3d11fb34b",
+    "url": "/Weathar-app/index.html"
+  },
+  {
+    "revision": "d93ccdfb9b14aa4c6040",
+    "url": "/Weathar-app/static/css/main.27c4c60a.chunk.css"
+  },
+  {
+    "revision": "8129369d1ce3b23b0a31",
+    "url": "/Weathar-app/static/js/2.abb76069.chunk.js"
+  },
+  {
+    "revision": "062222f47ec86680093a3a817014f059",
+    "url": "/Weathar-app/static/js/2.abb76069.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d93ccdfb9b14aa4c6040",
+    "url": "/Weathar-app/static/js/main.b97bfce4.chunk.js"
+  },
+  {
+    "revision": "73d60938d52eb23c80bd",
+    "url": "/Weathar-app/static/js/runtime-main.7dd4fdd8.js"
+  },
+  {
+    "revision": "e5747d17047cb82d9acb6f5cc256e05a",
+    "url": "/Weathar-app/static/media/back1.e5747d17.jpg"
+  },
+  {
+    "revision": "365370dc920af6bbd1391d8aebd37844",
+    "url": "/Weathar-app/static/media/sun.365370dc.svg"
+  }
+]);
